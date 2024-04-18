308adfc5748fe59f188bcaa4278a45c2020a2a2e
jbierc <j.bierc@icloud.com>
2024-04-18 12:08:43 +0200

    folder structure

---
+++ 102/Game/game.py
@@ -0,0 +1,23 @@
+"""
+Napisz prostą grę tekstową. Wymyśl prostą zagadkę, zaprojektuj jedno, max dwa pomieszczenia,
+dodaj możliwość zdobywania i używania przedmiotów. Pozwól nam przeżyć jakąś mini przygodę.
+
+use PRZEDMIOT
+take PRZEDMIOT
+items - wyświetli listę przedmiotów
+help - wyświetli listę dostępnych instrukcji
+describe - opisuje aktualne pomieszczenie
+
+Użyj klas i operacji wejścia/wyjścia.
+
+Potem zrób wpis na p-102-egzamin 
+Wpis ma zawierać 4 elementy:
+
+1. Napisz co sprawiło Ci największy problem i jak udało Ci się z tym poradzić.
+2. Daj link do nagrania rozgrywki.
+3. Daj link do repozytorium z kodem (publicznie dostępne).
+4. Jeśli do rozwiązania były Ci potrzebne inne materiały niż książka, wrzuć linki.
+"""
+
+from rooms import room1, room2, room3
+
